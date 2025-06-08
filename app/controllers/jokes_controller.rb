b8b1efe2954e74ea3749fed90a77ae1458fee444
Josh Levinson <joshua.levinson@gmail.com>
2025-06-07 21:41:16 -0400

    users

--- app/controllers/jokes_controller.rb
+++ app/controllers/jokes_controller.rb
@@ -1,5 +1,6 @@
 class JokesController < ApplicationController
   before_action :set_joke, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!
 
   # GET /jokes or /jokes.json
   def index
